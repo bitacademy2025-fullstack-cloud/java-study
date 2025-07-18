ea9b77b250b95af3884caff12a9101151df905ba
bit <bit@DESKTOP-RIH0NMM>
2025-07-18 12:34:13 +0900

    sol #1

--- drill05/src/main/java/drill05/prob01/Sol.java
+++ drill05/src/main/java/drill05/prob01/Sol.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class Sol {
 
 	public static void main(String[] args) {
+		final int ARRAY_LENGTH = 5;
+		
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		int[] numbers = new int[ARRAY_LENGTH];
+		for(int i = 0; i< ARRAY_LENGTH; i++) {
+			numbers[i] = scanner.nextInt();
+		}
+		
+		System.out.println(numbers.length);
 		
 		scanner.close();
 	}		
