b081ff170ea7a5f59706ff062ed28e6a4c7582c0
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-08-04 16:27:34 +0900

    socket options & alogorithm

--- network/src/main/java/network/test/TCPClient.java
+++ network/src/main/java/network/test/TCPClient.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class TCPClient {
 	private static final String SERVER_IP = "127.0.0.1";
@@ -15,6 +16,25 @@ public class TCPClient {
 		try {
 			// 1. Socket 생성
 			socket = new Socket();
+			
+			// 1-1. 소켓 버퍼 사이즈 확인
+			int rcvBufferSize = socket.getReceiveBufferSize();
+			int sndBufferSize = socket.getSendBufferSize();
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);
+
+			// 1-2. 소켓 버퍼 사이즈 변경
+			socket.setReceiveBufferSize(1024 * 10);
+			socket.setSendBufferSize(1024 * 10);
+			
+			rcvBufferSize = socket.getReceiveBufferSize();
+			sndBufferSize = socket.getSendBufferSize();
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);
+			
+			// 1-3. SO_NODELAY(Nagle Algorithm off)
+			socket.setTcpNoDelay(true);
+
+			// 1-4. SO_TIMEOUT
+			socket.setSoTimeout(2000); // 1 seconds
 			
 			// 2. 서버 연결
 			socket.connect(new InetSocketAddress(SERVER_IP, TCPServer.PORT));
@@ -39,6 +59,8 @@ public class TCPClient {
 			data = new String(buffer, 0, readByteCount, "utf-8");
 			System.out.println("[client] received: " + data);
 			
+		} catch(SocketTimeoutException e) {
+			System.out.println("[client] timeout!!");
 		} catch(IOException e) {
 			System.out.println("[client] error: " + e);
 		} finally {
